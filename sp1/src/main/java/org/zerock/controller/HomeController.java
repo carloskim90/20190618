9c467ea4effcf68c3b6af119eda4f1ad25022b0a
kim <kim@DESKTOP-KV8NV2T>
2019-06-18 16:56:08 +0900

    MySql Connection Test

--- sp1/src/main/java/org/zerock/controller/HomeController.java
+++ sp1/src/main/java/org/zerock/controller/HomeController.java
@@ -4,12 +4,16 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import javax.inject.Inject;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.zerock.service.HelloService;
 
 /**
  * Handles requests for the application home page.
@@ -19,11 +23,18 @@ public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
+	@Autowired//이 type에 bean이 있으면 좀 넣어줘
+	private HelloService service;
+	
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
+		
+		logger.info("----------------------------------------------");
+		logger.info(""+service);
+		logger.info("----------------------------------------------");
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
 		Date date = new Date();
